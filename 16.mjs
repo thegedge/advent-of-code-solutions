60d1f1199db2c3d85b089c02e867e91fa3d86fe2
Jason Gedge <jason@gadget.dev>
2022-12-18 13:03:37 -0330

    Day 16

--- 16.mjs
+++ 16.mjs
@@ -1,4 +1,4 @@
-import { max, minBy } from "lodash-es";
+import { range } from "lodash-es";
 import fs from "node:fs/promises";
 
 const data = await fs.readFile("./16.in", "utf8");
@@ -34,6 +34,20 @@ const readValves = (scenario) => {
     };
   });
 };
+
+function* combinations(array, n, from = 0) {
+  if (n == 0) {
+    yield [];
+    return;
+  }
+
+  for (let i = from; i <= array.length - n; ++i) {
+    const v = array[i];
+    for (const c of combinations(array, n - 1, i + 1)) {
+      yield [v, ...c];
+    }
+  }
+}
 
 // My first attempt was a naive backtracking solution, but it performed poorly.
 //
@@ -108,20 +122,6 @@ const floyd_warshall = (valves) => {
   }
 };
 
-function* combinations(array, n, from = 0) {
-  if (n == 0) {
-    yield [];
-    return;
-  }
-
-  for (let i = from; i <= array.length - n; ++i) {
-    const v = array[i];
-    for (const c of combinations(array, n - 1, i + 1)) {
-      yield [v, ...c];
-    }
-  }
-}
-
 // My second approach was observing that a lot of valves have no flow. An observation here is that we'd never open
 // these valves, so they only serve as a means to travel to valves that can produce flow.
 //
@@ -129,56 +129,71 @@ function* combinations(array, n, from = 0) {
 // all of the other valves that have flow. That way we never have to recurse into those valves, and simply jump
 // to them and subtract the amount of time it would take us to do so.
 //
+// Another thing I do now is "for..let index" loops, which are significantly faster than forEach and "for const of"
+// loops, and given how much backtracking we're doing, this is a significant and noticeable speed up. This is generally
+// not something one should worry about, but this is a REALLY hot loop, so it makes a difference. We also avoid
+// the cost of a destructuring assignment in a hot loop.
+//
 const solve = (valves, time, n) => {
   floyd_warshall(valves);
 
   const flowValves = valves.filter((v) => v.rate > 0);
-  const combos = Array.from(combinations(flowValves, n));
 
-  const solve_ = (peeps, rate, available) => {
-    // If we've already opened every flow-y valve, or there's no time left for anyone, the best we can do is the current rate
-    if (available == 0 || peeps.every((v) => v[1] <= 1)) {
-      return rate;
+  const solve_ = (peeps, rate, available, onlyPeepsAfterIndex = 0) => {
+    let best = rate;
+
+    // If we've already opened every flow-y valve, the best we can do is the current rate
+    if (available == 0) {
+      return best;
     }
 
-    // Always move the peep that has more time left
-    const peep = minBy(peeps, (v) => v[1]);
-    const valve = valves[peep[0]];
+    // "for..let index" loops are significantly faster than forEach and "for const of" loops, and given how much backtracking
+    // we're doing, this is a significant and noticeable speed up. We also avoid destructuring assignment.
+    for (let fi = 0; fi < flowValves.length; ++fi) {
+      const flowValve = flowValves[fi];
+      if (flowValve.open) {
+        continue;
+      }
 
-    let best = rate;
-    if (!valve.open) {
-      valve.open = true;
-      rate += (peep[1] - 1) * valve.rate;
-
-      for (const { index } of flowValves) {
-        if (index == valve.index) {
+      // An observation I made after some time was that the backtracking solution would do a lot of duplicate branches. This was the
+      // last piece that allowed me to have a solution that finished in a reasonable amount of time (16s, still pretty slow).
+      //
+      // The idea here is that if we always start from index 0, we will have the following recursion (for n=2)
+      //
+      //   0 visits AA -> 1 visits CC -> 0 visits BB
+      //   0 visits AA -> 0 visits BB -> 1 visits CC
+      //
+      // The result for both of these will be the same. Instead, we heavily prune by only considering all peeps after the index we're
+      // currently considering.
+      //
+      for (let pi = onlyPeepsAfterIndex; pi < peeps.length; ++pi) {
+        const peep = peeps[pi];
+        const valve = valves[peep[0]];
+        const moveAndOpenCost = 1 + valve.distances[flowValve.index];
+        if (peep[1] <= moveAndOpenCost) {
           continue;
         }
 
-        peep[0] = index;
-        peep[1] -= 1 + valve.distances[index];
+        valves[flowValve.index].open = true;
+        peep[0] = flowValve.index;
+        peep[1] -= moveAndOpenCost;
 
-        // Recurse under the scenario that we open the current valve and travel to the given flow valve. Since we're
-        // opening this valve, we flag it as such and then add the new flow rate. We take 1 minute away for opening
-        // the valve, then the distance to the destination valve.
-        best = Math.max(best, solve_(peeps, rate, available - 1));
+        const newRate = rate + peep[1] * flowValve.rate;
 
+        // Consider the scenario where we move to and open the given valve
+        best = Math.max(best, solve_(peeps, newRate, available - 1, pi));
+
+        valves[flowValve.index].open = false;
         peep[0] = valve.index;
-        peep[1] += 1 + valve.distances[index];
+        peep[1] += moveAndOpenCost;
       }
-
-      valve.open = false;
     }
 
     return best;
   };
 
-  return max(
-    combos.map((combo) => {
-      const peeps = combo.map(({ index }) => [index, time - valves[0].distances[index]]);
-      return solve_(peeps, 0, flowValves.length);
-    })
-  );
+  const peeps = range(n).map(() => [0, time]);
+  return solve_(peeps, 0, flowValves.length);
 };
 
 // Part 1
