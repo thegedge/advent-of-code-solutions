1f17f32c4bdfb011b5ced9f740a8bf0cdef28a3a
Jason Gedge <jason@gadget.dev>
2022-12-19 14:32:37 -0330

    [skip ci] temp

--- 17.mjs
+++ 17.mjs
@@ -6,11 +6,11 @@ const data = await fs.readFile("./17.in", "utf8");
 // My first attempt represented it as multidimensional arrays of booleans. I realized this could be
 // much faster by doing boolean operations on numbers. If A & B != 0, they intersect.
 const shapes = [
-  [4, [0b1111000]],
-  [3, [0b0100000, 0b1110000, 0b0100000]],
-  [3, [0b0010000, 0b0010000, 0b1110000]],
-  [1, [0b1000000, 0b1000000, 0b1000000, 0b1000000]],
-  [2, [0b1100000, 0b1100000]],
+  [4, [0b11110000]],
+  [3, [0b01000000, 0b11100000, 0b01000000]],
+  [3, [0b00100000, 0b00100000, 0b11100000]],
+  [1, [0b10000000, 0b10000000, 0b10000000, 0b10000000]],
+  [2, [0b11000000, 0b11000000]],
 ];
 
 const MAP_WIDTH = 7;
@@ -18,57 +18,104 @@ const MAP_RANGE = range(MAP_WIDTH);
 
 const isBitSet = (v, n) => (v & (1 << (7 - n))) != 0;
 
-const dump = (map, shape, x, y) => {
-  const maps = [
-    ...range(y < 0 ? -y : 0).map(() => ".".repeat(7)),
-    ...map.map((bitmask) => {
-      return MAP_RANGE.map((v) => (isBitSet(bitmask, v) ? "#" : ".")).join("");
-    }),
-  ];
-
-  if (shape) {
-    const sy = y < 0 ? 0 : y;
-    shape.forEach((bitmask, row) => {
-      MAP_RANGE.forEach((v, col) => {
-        if (isBitSet(bitmask, v >> x)) {
-          const s = maps[sy + row];
-          maps[sy + row] = s.substring(0, x + col) + "@" + s.substring(x + col + 1);
-        }
-      });
-    });
+class Map {
+  constructor(max = 5000) {
+    this.y = 0;
+    this.lo = 0;
+    this.hi = 0;
+    this.data = new Uint8ClampedArray(max);
+    this.data.fill(0xff);
   }
 
-  console.log(maps.join("\n") + "\n");
-};
+  unshift() {
+    this.lo = (this.lo - 1 + this.length) % this.length;
+    this.data[this.lo] = 0;
 
-const simulate = (group, N) => {
-  const deltas = group.split("").map((v) => (v == "<" ? -1 : 1));
-  const map = [];
+    if (this.lo == this.hi) {
+      this.hi = (this.hi - 1 + this.length) % this.length;
+    }
+  }
 
-  // TODO lookup table for intersection tests
-  const intersectShape = (shape, x, y) => {
+  get length() {
+    return this.data.length;
+  }
+
+  test(shape, x, y) {
     if (x < 0 || x + shape[0] > MAP_WIDTH) {
       return true;
     }
 
     // Small optimization: go in reverse, because we're likely to intersect on the bottom
-    for (let row = shape[1].length - 1, index = y + row; row >= 0; --row, --index) {
-      if (index < 0) {
+    for (let row = shape[1].length - 1; row >= 0; --row) {
+      if (y + row < 0) {
         return false;
       }
 
-      if (index < map.length && ((shape[1][row] >> x) & map[index]) != 0) {
+      if (((shape[1][row] >> x) & this.get(y + row)) != 0) {
         return true;
       }
     }
 
     return false;
-  };
+  }
+
+  get(row) {
+    return this.data[(this.lo + row) % this.length];
+  }
+
+  set(shape, x, y) {
+    let row = 0;
+
+    y++ >= 0 && this.union(row++, shape[1][0] >> x);
+    if (shape[1][1]) {
+      y++ >= 0 && this.union(row++, shape[1][1] >> x);
+      if (shape[1][2]) {
+        y++ >= 0 && this.union(row++, shape[1][2] >> x);
+        if (shape[1][3]) {
+          y++ >= 0 && this.union(row++, shape[1][3] >> x);
+        }
+      }
+    }
+  }
+
+  union(row, value) {
+    this.data[(this.lo + row) % this.data.length] |= value;
+  }
+
+  dump(shape, x, y) {
+    const maps = range(y < 0 ? -y : 0).map(() => ".".repeat(7));
+
+    let index = this.lo;
+    while (index != this.hi) {
+      const bitmask = this.data[index];
+      maps.push(MAP_RANGE.map((v) => (isBitSet(bitmask, v) ? "#" : ".")).join(""));
+      index = (index + 1) % this.data.length;
+    }
+
+    if (shape) {
+      const sy = y < 0 ? 0 : y;
+      shape[1].forEach((bitmask, row) => {
+        MAP_RANGE.forEach((v, col) => {
+          if (isBitSet(bitmask >> x, v)) {
+            const s = maps[sy + row];
+            maps[sy + row] = s.substring(0, col) + "@" + s.substring(col + 1);
+          }
+        });
+      });
+    }
+
+    console.log(maps.join("\n") + "\n");
+  }
+}
+
+const simulate = (group, N) => {
+  const deltas = group.split("").map((v) => (v == "<" ? -1 : 1));
+  const map = new Map();
 
   let height = 0;
   let deltaIndex = 0;
   const start = performance.now();
-  const iters = Math.min(N, 1_000_000);
+  const iters = Math.min(N, 10_000_000);
   for (let rock = 0; rock < iters; ++rock) {
     const shape = shapes[rock % shapes.length];
     const shapeHeight = shape[1].length;
@@ -77,7 +124,9 @@ const simulate = (group, N) => {
     let y = -3 - shapeHeight;
 
     while (true) {
-      if (!intersectShape(shape, x + deltas[deltaIndex], y)) {
+      // map.dump(shape, x, y);
+
+      if (!map.test(shape, x + deltas[deltaIndex], y)) {
         x += deltas[deltaIndex];
       }
 
@@ -85,31 +134,26 @@ const simulate = (group, N) => {
       deltaIndex = (deltaIndex + 1) % deltas.length;
 
       if (y + shapeHeight == height) {
+        // console.log("grounded");
         break;
       }
 
       // If we're still in the empty space where we started, don't do the intersection
-      if (y + shapeHeight >= 0 && intersectShape(shape, x, y + 1)) {
+      if (y + shapeHeight >= 0 && map.test(shape, x, y + 1)) {
+        // console.log("intersected");
         break;
       }
 
       ++y;
     }
 
-    if (map.length > 1000) {
-      map.splice(200);
-    }
-
     while (y < 0) {
-      map.unshift(0);
+      map.unshift();
       ++height;
       ++y;
     }
 
-    shape[1][0] && (map[y + 0] |= shape[1][0] >> x);
-    shape[1][1] && (map[y + 1] |= shape[1][1] >> x);
-    shape[1][2] && (map[y + 2] |= shape[1][2] >> x);
-    shape[1][3] && (map[y + 3] |= shape[1][3] >> x);
+    map.set(shape, x, y);
   }
 
   const usPerIter = (1000 * (performance.now() - start)) / iters;
@@ -119,18 +163,21 @@ const simulate = (group, N) => {
 };
 
 // Part 1
-
 data
   .split("\n")
-  .map((group) => {
+  .map((group, index) => {
+    if (index == 1) {
+      return 0;
+    }
     return simulate(group, 2022);
   })
   .forEach((v) => console.log(v));
 
 // Part 2
-data
-  .split("\n")
-  .map((group) => {
-    return simulate(group, 1_000_000_000_000);
-  })
-  .forEach((v) => console.log(v));
+// data
+//   .split("\n")
+//   .map((group, index) => {
+//     if (index == 1) return 0;
+//     return simulate(group, 1_000_000_000_000);
+//   })
+//   .forEach((v) => console.log(v));
