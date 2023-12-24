b4b0a2be716ddc69da9e18f3c605bdfda2cad3d5
Jason Gedge <jason@gadget.dev>
2023-12-24 13:16:02 -0500

    Solution for problem 1, 2023

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Jason Gedge's solutions to Advent of Code";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system: nixpkgs.lib.fix (flake:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          callPackage = pkgs.newScope (flake.packages // { inherit callPackage; });
+        in
+        {
+          packages = {
+            direnv = pkgs.direnv;
+            nix-direnv = pkgs.nix-direnv;
+
+            deno = pkgs.deno;
+            nodejs = pkgs.nodejs_21;
+            corepack = pkgs.corepack;
+
+            rust = pkgs.rust;
+          };
+
+          devShell = callPackage ./devShell.nix {
+            mkShell = pkgs.mkShellNoCC;
+            packages = flake.packages;
+          };
+        }));
+}
